3b946db24732a75d34c14396f0fd7771d90b15d3
AndriiPetrechko <petrechkoandrij@gmail.com>
2020-08-18 09:59:24 +0300

    Update 2020-08-18T06:59:18.015Z

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
-$(document).ready(function(){
-    $('.tab_item').click(function(){
-        $('.active_tab').removeClass('active_tab');
-        $(this).toggleClass('active_tab');
-    })
+"use strict";
 
-    // collapse menu rotate
-    $('.burger_menu').click(function(){
-        $(this).toggleClass('burger_active');
-        $('.collapse_menu').toggleClass('collapse_menu_active');
-    })
-})
-$(document).ready(function(){
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 200) {
-            $(".header").addClass("active_header");
-        } else {
-            //remove the background property so it comes transparent again (defined in your css)
-           $(".header").removeClass("active_header");
-        }
-    });
-})
+$(document).ready(function () {
+  $('.tab_item').click(function () {
+    $('.active_tab').removeClass('active_tab');
+    $(this).toggleClass('active_tab');
+  }); // collapse menu rotate
 
-
+  $('.burger_menu').click(function () {
+    $(this).toggleClass('burger_active');
+    $('.collapse_menu').toggleClass('collapse_menu_active');
+  });
+});
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 200) {
+      $(".header").addClass("active_header");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $(".header").removeClass("active_header");
+    }
+  });
+});
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
+gsap.to("#Ov", {
+  scrollTrigger: {
+    trigger: "#Ov",
+    start: "top center",
+    end: "top 100px",
+    scrub: 5
+  },
+  motionPath: {
+    path: "Path",
+    align: "Path",
+    alignOrigin: [0.5, 0.5]
+  },
+  duration: 3,
+  ease: "none"
+});
