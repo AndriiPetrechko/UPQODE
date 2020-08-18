8b520a21b566cabacf152458bb286caa08bf7c4d
AndriiPetrechko <petrechkoandrij@gmail.com>
2020-08-18 22:43:50 +0300

    next commit

--- #src/js/youtube_video.js
+++ #src/js/youtube_video.js
@@ -11,8 +11,7 @@ function onYouTubePlayerAPIReady() {
     width: '100%',
     videoId: '668nUCeBHyY',
     events: {
-        "onReady": onPlayerReady,
-        'onStateChange': onPlayerStateChange
+        "onReady": onPlayerReady
     }
   });
 }
